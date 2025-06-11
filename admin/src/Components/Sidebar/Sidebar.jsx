7b3d857b19447cb82a2235603247128350fcec1a
vasanth-vkjack <vasanthvk694@gmail.com>
2025-06-11 19:07:09 +0530

    someChanges

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './sidebar.css'
 import { Link } from 'react-router-dom'
-import add_product_icon from '../../assets/Product_cart.svg'
+import add_product_icon from '../../assets/Product_list_icon.svg'
 import list_product_icon from '../../assets/Product_list_icon.svg'
 
 const Sidebar = () => {
